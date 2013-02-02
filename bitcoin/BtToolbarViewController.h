b27bea5bbc70236c5a08e59775df87327ab30667
Kevin Greene <kgreenek@gmail.com>
2013-02-02 11:57:10 -0800

    Adding BtToolbarViewController class.

---
+++ bitcoin/BtToolbarViewController.h
@@ -0,0 +1,13 @@
+//
+//  BtToolbarViewController.h
+//  bitcoin
+//
+//  Created by Kevin Greene on 2/2/13.
+//  Copyright (c) 2013 Kevin Greene. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface BtToolbarViewController : NSObject
+
+@end
---
+++ bitcoin/BtToolbarViewController.m
@@ -0,0 +1,13 @@
+//
+//  BtToolbarViewController.m
+//  bitcoin
+//
+//  Created by Kevin Greene on 2/2/13.
+//  Copyright (c) 2013 Kevin Greene. All rights reserved.
+//
+
+#import "BtToolbarViewController.h"
+
+@implementation BtToolbarViewController
+
+@end
